79574b82b7572a65a5fc4d4c402080d01e90ae8c
HarshKhandeparkar <mjkgoa@gmail.com>
2020-09-26 02:25:22 +0530

    fix: use async...await

--- src/util/askDM.ts
+++ src/util/askDM.ts
@@ -7,40 +7,38 @@ import DiscordClient from './DiscordClient';
  * @param {User} askTo Disccord.js user object of the user to ask.
  * @param {String} question The question as a string.
  * @param {function} onAnswerCb Callback which is fired if the user answers the question. With the answer and msg object as parameters.
- * @param {function} onNotAnswerCb 
+ * @param {function} onNotAnswerCb
  */
-function askDM(
+async function askDM(
   client: DiscordClient,
   askTo: User,
   question: string,
   onAnswerCb: Function = (ans: string, msg: Message) => console.log(`Answered ${ans} by <@${msg.author.id}>`),
   onNotAnswerCb: Function = () => console.log('Not Answered')
 ) {
-  askTo.send(`<@${askTo.id}> ${question}`).then(dm => {
-    const notAnsweredHandler = () => {
-      askTo.send(`<@${askTo.id}> You didn't answer in 30s, now your chance is gone.`);
-      onNotAnswerCb();
+  const dm = await askTo.send(`<@${askTo.id}> ${question}`);
+  const notAnsweredHandler = () => {
+    askTo.send(`<@${askTo.id}> You didn't answer in 30s, now your chance is gone.`);
+    onNotAnswerCb();
+    client.offMsg(`dm-${question}@${askTo.id}#${dm.channel.id}`);
+  }
+  let notAnsweredTimeout: NodeJS.Timeout;
+
+  const finalAnswerHandler = (msg: Message) => {
+    if (msg.author.id === askTo.id && msg.channel.id === dm.channel.id) {
+      const answer = msg.content;
+
+      clearTimeout(notAnsweredTimeout);
       client.offMsg(`dm-${question}@${askTo.id}#${dm.channel.id}`);
+      onAnswerCb(answer, msg);
     }
-    let notAnsweredTimeout: NodeJS.Timeout;
+  }
 
-    const finalAnswerHandler = (msg: Message) => {
-      if (msg.author.id === askTo.id && msg.channel.id === dm.channel.id) {
-        const answer = msg.content;
-        
-        clearTimeout(notAnsweredTimeout);
-        client.offMsg(`dm-${question}@${askTo.id}#${dm.channel.id}`);
-        onAnswerCb(answer, msg);  
-      }
-    }
-
-    notAnsweredTimeout = setTimeout(notAnsweredHandler, 30000);
-    client.onMsg({
-      name: `dm-${question}@${askTo.id}#${dm.channel.id}`,
-      handler: finalAnswerHandler
-    })
+  notAnsweredTimeout = setTimeout(notAnsweredHandler, 30000);
+  client.onMsg({
+    name: `dm-${question}@${askTo.id}#${dm.channel.id}`,
+    handler: finalAnswerHandler
   })
-
 }
 
 export default askDM;
