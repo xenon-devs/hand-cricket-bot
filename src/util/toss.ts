124aa0b916f7bffc1337d1447572a0b1309c5583
HarshKhandeparkar <mjkgoa@gmail.com>
2020-09-26 13:30:51 +0530

    fix: refactor ongoing

--- src/util/toss.ts
+++ src/util/toss.ts
@@ -4,7 +4,7 @@ import DiscordClient from './DiscordClient';
 
 const completeToss = (playerToss: string, cb?: (tossWon: boolean) => void) => {
   const myToss = Math.round(Math.random());
-  
+
   const toss = playerToss === 'heads' ? 0 : 1;
 
   if (toss === myToss) cb(false);
@@ -28,19 +28,19 @@ const tossCheckHandler = (client: DiscordClient, player: User, channel: TextChan
       break;
     default:
       ask(client, player, channel,'Can\'t you answer heads or tails? Useless fellow.',  answer => tossCheckHandler(client, player, channel, answer, cb))
-      break; 
+      break;
   }
 }
 
 /**
- * 
- * @param {User} player Discord.js User object of the player who selects the toss.
- * @param {Client} client The main discord.js client object.
- * @param {TextChannel} channel The channel in which the coin is flipped.
- * @param {function} cb A callback that is run when the toss completes. The only parameter is a boolean which is true when the player loses (bot wins).
+ *
+ * @param player Discord.js User object of the player who selects the toss.
+ * @param client The main discord.js client object.
+ * @param channel The channel in which the coin is flipped.
+ * @param cb A callback that is run when the toss completes. The only parameter is a boolean which is true when the player loses (bot wins).
  */
 const toss = (player: User, client: DiscordClient, channel: TextChannel, cb?: Function) => {
-  channel.send('TOSS:');  
+  channel.send('TOSS:');
   ask(client, player, channel, 'Heads or Tails?', (answer: string) => tossCheckHandler(client, player, channel, answer, cb));
 }
 
