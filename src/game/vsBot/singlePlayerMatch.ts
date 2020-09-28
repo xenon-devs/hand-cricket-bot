fa414d29ecaa4dd6b8dc00df21ded0f1e764ea49
HarshKhandeparkar <mjkgoa@gmail.com>
2020-09-28 13:34:41 +0530

    [WIP] Single Player Refactor

---
+++ src/game/vsBot/singlePlayerMatch.ts
@@ -0,0 +1,19 @@
+import { Match, Players } from '../../util/match';
+import DiscordClient from '../../util/DiscordClient';
+import { TextChannel, User } from 'discord.js';
+
+export class SinglePlayerMatch extends Match {
+  constructor(client: DiscordClient, stadium: TextChannel, challenger: User) {
+    super(client, stadium, challenger);
+    this.opponent = client.user;
+  }
+
+  startMatch() {
+    this.toss(this.challenger, this.client, this.stadium, (botWonToss: boolean) => {
+      this.opener = botWonToss ? Players.OPPONENT : Players.CHALLENGER;
+      this.currentBatsman = botWonToss ? Players.OPPONENT : Players.CHALLENGER;
+
+      
+    })
+  }
+}
