4754230ed42deaf7626d21d60a1d9f7a08a9cb7d
HarshKhandeparkar <mjkgoa@gmail.com>
2020-09-30 13:22:51 +0530

    fix: refactor ask.ts

--- src/util/ask.ts
+++ src/util/ask.ts
@@ -1,46 +1,47 @@
-import { User, TextChannel, Message, DMChannel } from "discord.js";
-import DiscordClient from "./DiscordClient";
-
-type onAnswerCb = (answer: string, msg: Message) => void;
+import { User, TextChannel, DMChannel, Message } from 'discord.js';
+import DiscordClient from './DiscordClient';
 
 /**
  * @description Ask a question to a specific discord user and wait for the answer in a specific channel.
- * @param {DiscordClient} client The main discord.js client object.
- * @param {User} askTo Discord.js user object of the user to ask.
- * @param {TextChannel} channel Discord.js channel to ask the question in.
- * @param {String} question Question as a string (including ?).
- * @param {function} onAnswerCb Callback that fires when the answer is received. The only parameter is answer (string).
+ * @param client The main discord.js client object.
+ * @param askTo Discord.js user object of the user to ask.
+ * @param channel Discord.js channel to ask the question in.
+ * @param question Question as a string (including ?).
+ * @param timeout Timeout in ms, default 20000.
  */
-function ask(
+export async function ask(
   client: DiscordClient,
   askTo: User,
   channel: TextChannel | DMChannel,
   question: string,
-  onAnswerCb: onAnswerCb = console.log
+  timeout = 20000
 ) {
-  channel.send(`<@${askTo.id}> ${question}`);
-
-  const notAnsweredHandler = () => {
-    channel.send(`<@${askTo.id}> You didn't answer in 20s, now your chance is gone.`)
-    client.offMsg(`${question}@${askTo.id}#${channel.id}`);
-  }
-  let notAnsweredTimeout: NodeJS.Timeout;
-
-  const finalAnswerHandler = (msg: Message, timeout: NodeJS.Timeout) => {
-    if (msg.author.id === askTo.id && msg.channel.id === channel.id) {
-      const answer = msg.content;
-
-      clearTimeout(timeout);
+  return new Promise((resolve: (value: {answer: string, msg: Message}) => void, reject) => {
+    channel.send(`<@${askTo.id}> ${question}`);
+    const notAnsweredHandler = async () => {
+      channel.send(`<@${askTo.id}> You didn't answer in ${timeout / 1000}s, now your chance is gone.`);
       client.offMsg(`${question}@${askTo.id}#${channel.id}`);
-      onAnswerCb(answer, msg);
-    }
-  }
 
-  notAnsweredTimeout = setTimeout(notAnsweredHandler, 20000);
-  client.onMsg({
-    name: `${question}@${askTo.id}#${channel.id}`,
-    handler: msg => finalAnswerHandler(msg, notAnsweredTimeout)
+      reject(new Error('Did not answer'));
+    }
+    let notAnsweredTimeout: NodeJS.Timeout;
+    notAnsweredTimeout = setTimeout(notAnsweredHandler, timeout);
+
+    const finalAnswerHandler = (msg: Message) => {
+      if (msg.author.id === askTo.id && msg.channel.id === channel.id) {
+        const answer = msg.content;
+
+        clearTimeout(notAnsweredTimeout);
+        client.offMsg(`${question}@${askTo.id}#${channel.id}`);
+        resolve({
+          answer,
+          msg
+        })
+      }
+    }
+    client.onMsg({
+      name: `${question}@${askTo.id}#${channel.id}`,
+      handler: finalAnswerHandler
+    })
   })
 }
-
-export default ask;
