d06607f6c300e3bbffb7ef7b066982df1a14abe8
HarshKhandeparkar <mjkgoa@gmail.com>
2020-11-21 23:51:02 +0530

    feat: added leaderboard

--- src/game/match/match.ts
+++ src/game/match/match.ts
@@ -76,6 +76,22 @@ export class Match {
     else if (this.openerScore === this.chaserScore) this.result = MatchResult.TIE;
     else if (this.openerScore < this.chaserScore) this.result = this.opener === Players.CHALLENGER ? MatchResult.OPPONENT_WON : MatchResult.CHALLENGER_WON;
 
+    // Handle high scores leaderboard
+    if (this.result == MatchResult.CHALLENGER_WON || this.result === MatchResult.OPPONENT_WON) {
+      const winnerScore = Math.max(this.openerScore, this.chaserScore);
+      const winner = this.result === MatchResult.CHALLENGER_WON ? this.challenger : this.opponent;
+
+      if (!winner.bot) {
+        this.client.highScoreDB.addHighScore(
+          {
+            score: winnerScore,
+            tag: winner.tag
+          },
+          !this.opponent.bot // Multiplayer if opponent is not bot
+        )
+      }
+    }
+
     this.stadium.send(this.getScoreBoard());
     return this.matchEndedCb();
   }
