eba66c058f4e95f80f2f732f8009075a1296166f
HarshKhandeparkar <mjkgoa@gmail.com>
2020-11-24 13:10:00 +0530

    fix: make rules command fields inline

--- src/commands/commands/rules.ts
+++ src/commands/commands/rules.ts
@@ -14,9 +14,9 @@ export function setRules(client: DiscordClient) {
         .setTitle('Extremely Official Rules of Hand Cricket')
         .setDescription('One player bowls and the other bats. The player has to type any number between 0 and 6(representing the number of fingers), once the player enters, the bot will generate a random number as it\'s output.')
         .addFields(
-          { name: '1. ', value: 'If the number of fingers are equal, the batsman is out.' },
-          { name: '2. ', value: 'If the number of fingers do not match, the number of fingers on the batsman is the number of runs scored.' },
-          { name: '3. ', value: 'All rules of cricket apply.' }
+          { name: '1. ', value: 'If the number of fingers are equal, the batsman is out.', inline: true },
+          { name: '2. ', value: 'If the number of fingers do not match, the number of fingers on the batsman is the number of runs scored.', inline: true },
+          { name: '3. ', value: 'All rules of cricket apply.', inline: true }
         )
         .setThumbnail(client.user.displayAvatarURL())
         .setTimestamp()
