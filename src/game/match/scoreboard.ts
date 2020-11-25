514273421827cea10d38dd60d5a31310f9a0ce0f
HarshKhandeparkar <mjkgoa@gmail.com>
2020-11-25 11:18:20 +0530

    fix: fix

--- src/game/match/scoreboard.ts
+++ src/game/match/scoreboard.ts
@@ -64,7 +64,7 @@ export function getScoreboard(
     }
 
     if (this.client.dblIntegration) {
-      scoreboard.addField('If you enjoyed', `Please [vote](https://top.gg/bot/${this.client.user.id}).`);
+      if(this.result === MatchResult.CHALLENGER_WON || this.result === MatchResult.OPPONENT_WON || this.result === MatchResult.TIE) scoreboard.addField('If you enjoyed', `Please [vote](https://top.gg/bot/${this.client.user.id}).`);
     }
 
     return scoreboard;
