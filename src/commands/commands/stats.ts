98accdbeb677147502822add8a3a6702ecad5e92
HarshKhandeparkar <mjkgoa@gmail.com>
2020-11-25 11:32:38 +0530

    fix: rearrange stats embed

--- src/commands/commands/stats.ts
+++ src/commands/commands/stats.ts
@@ -17,8 +17,6 @@ export function setStats(
         .setTitle('Hand Cricketer Stats')
         .addField('Servers', `\`${client.guilds.cache.array().length}\``, true)
         .addField('Users', `\`${client.guilds.cache.array().map(guild => guild.memberCount).reduce((a, b) => a + b)}\``, true)
-        .addField('1P Matches Played', `\`${matchesPlayed.singlePlayer}\``, true)
-        .addField('2P Matches Played', `\`${matchesPlayed.multiPlayer}\``, true)
         .setThumbnail(client.user.displayAvatarURL())
         .setColor('RED');
 
@@ -26,7 +24,16 @@ export function setStats(
         const botStats = await client.dbl.getBot(client.user.id);
         statsEmbed.addField(`top.gg votes`, `\`${botStats.points}\``, true);
 
+        statsEmbed
+          .addField('1P Matches Played', `\`${matchesPlayed.singlePlayer}\``, true)
+          .addField('2P Matches Played', `\`${matchesPlayed.multiPlayer}\``, true)
+
         if (botStats.invite) statsEmbed.addField('Liked the bot?', `[Vote it!](https://top.gg/bot/${client.user.id}/vote)`);
+      }
+      else {
+        statsEmbed
+          .addField('1P Matches Played', `\`${matchesPlayed.singlePlayer}\``, true)
+          .addField('2P Matches Played', `\`${matchesPlayed.multiPlayer}\``, true)
       }
 
       msg.channel.send(statsEmbed);
