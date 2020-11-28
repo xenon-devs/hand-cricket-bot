c22428e72ca635e397d37bc5935b7f0aa62aa7a7
HarshKhandeparkar <mjkgoa@gmail.com>
2020-11-28 10:58:18 +0530

    fix: slightly change the ad

--- src/tournament/tournamentAdvertisement.ts
+++ src/tournament/tournamentAdvertisement.ts
@@ -6,16 +6,17 @@ export async function getTournamentAdvertisementEmbed(client: DiscordClient) {
     const botInfo = await client.dbl.getBot(client.user.id);
 
     return new MessageEmbed()
-      .setTitle(`Hand Cricketer Tournament :trophy:`)
+      .setTitle(`Hand Cricket Tournament :trophy:`)
       .setThumbnail(client.user.displayAvatarURL())
-      .setFooter(`Hand Cricketer Tournament`)
+      .setFooter(`Hand Cricket Tournament`)
       .setTimestamp()
       .setColor('#4400cc')
       .setDescription(`
 We are hosting a global Hand Cricket <:handcric:763782166202417173> tournament!
 If you love the bot or the game, please consider participating. (it's free!)
 If you are on the global leaderboard, PLEASE DO PARTICIPATE!
-[Join the support server](https://discord.gg/${botInfo.support}) for more details.
+[Click](https://discord.gg/${botInfo.support}) to join the support server for more details and to participate.
 `)
+      .addField('To participate', `[Click](https://discord.gg/${botInfo.support}) to join support server`)
   }
 }
