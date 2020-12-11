95b88c308845bbcf6985e4c84b29b0cef7b36fa7
HarshKhandeparkar <mjkgoa@gmail.com>
2020-12-12 00:06:48 +0530

    fix: make high score more apparent

--- src/commands/commands/leaderboard.ts
+++ src/commands/commands/leaderboard.ts
@@ -30,14 +30,14 @@ export function setLeaderboard(client: DiscordClient) {
         .addField(
           'Single Player',
           scores.singlePlayer.length > 0 ?
-            'Following is the list of top single player batsmen.' :
+            'Following is the list of top single player high scores.' :
             'No scores recorded yet.',
           false)
         .addFields(singlePlayerRankFields)
         .addField(
           'Multi Player',
           scores.multiPlayer.length > 0 ?
-            'Following is the list of top multi player batsmen.' :
+            'Following is the list of top multi player high scores.' :
             'No scores recorded yet.',
           false
         )
