87799d3468fa97b944f1117204627b8bd77a8703
HarshKhandeparkar <mjkgoa@gmail.com>
2020-12-26 12:12:30 +0530

    fix: Add global matches in the ad

--- src/advertisement/advertisement.ts
+++ src/advertisement/advertisement.ts
@@ -2,23 +2,14 @@ import { MessageEmbed } from 'discord.js';
 import { DiscordClient } from '../util/discord-client';
 
 export async function getAdvertisementEmbed(client: DiscordClient) {
-  if (client.dblIntegration) {
-    const botInfo = await client.dbl.getBot(client.user.id);
-
     return new MessageEmbed()
-      .setTitle(`Crick-O-Dile League Season 1 :trophy:`)
+      .setTitle(`Global Matches!`)
       .setThumbnail(client.user.displayAvatarURL())
-      .setFooter(`Hand Cricket Tournament`)
+      .setFooter(`New Feature`)
       .setTimestamp()
       .setColor('#4400cc')
       .setDescription(`
-We are hosting a global Hand Cricket <:handcric:763782166202417173> tournament named the **Crick-O-Dile League**!
-If you love the bot or the game, please consider participating. (it's free!)
-If you are on the global leaderboard, PLEASE DO PARTICIPATE!
-The tournament will be held in the [Support Server](https://discord.gg/${botInfo.support}).
+Now you can play matches against any player in any server globally! Just use the \`global\` command and see the magic!
+You might have to wait for a while for a match to be found but as the popularity of this feature grows, you will find a match almost instantly.
 `)
-      .addField('Date', '5 Dec 2020', true)
-      .addField('Registration Deadline', '3 Dec 2020', true)
-      .addField('To participate', `[Click](https://discord.gg/${botInfo.support}) to join support server`, false)
-  }
 }
