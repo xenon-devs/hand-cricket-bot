8bc1249ae6c32ea3432b48ed3ca19cb37a80bfe6
HarshKhandeparkar <mjkgoa@gmail.com>
2020-12-27 11:33:48 +0530

    fix<global>: convert scoreboard @mentions to usernames

--- src/game/global/global.ts
+++ src/game/global/global.ts
@@ -1,8 +1,9 @@
 import { MultiPlayerMatch } from '../multi-player/multi-player';
-import { GameMode } from '../match/match';
+import { GameMode, Players, MatchResult } from '../match/match';
 import { startMatch } from './start-match';
 import { DiscordClient } from '../../util/discord-client';
 import { TextChannel, User } from 'discord.js';
+import { runInThisContext } from 'vm';
 
 export class GlobalMatch extends MultiPlayerMatch {
   gameMode: GameMode.TEST_MATCH;
@@ -34,6 +35,37 @@ export class GlobalMatch extends MultiPlayerMatch {
   }
 
   protected sendScoreBoard() {
+    this.scoreboard = this.generateScoreBoard();
+
+    // Convert @mentions to usernames
+    const openerFieldIndex = this.scoreboard.fields.findIndex(field => field.name === 'Opener');
+    this.scoreboard.fields[openerFieldIndex].value = this.opener ? (this.opener === Players.OPPONENT ? `**${this.opponent.username}**` : `**${this.challenger.username}**`) : ':question:';
+
+    const chaserFieldIndex = this.scoreboard.fields.findIndex(field => field.name === 'Chaser');
+    this.scoreboard.fields[chaserFieldIndex].value = this.opener ? (this.opener === Players.CHALLENGER ? `**${this.opponent.username}**` : `**${this.challenger.username}**`) : ':question:';
+
+    const resultFieldIndex = this.scoreboard.fields.findIndex(field => field.name === 'Result');
+    switch (this.result) {
+      case MatchResult.OPPONENT_WON:
+        this.scoreboard.fields[resultFieldIndex].value = `**${this.opponent.username}** won! :trophy:`;
+        break;
+      case MatchResult.CHALLENGER_WON:
+        this.scoreboard.fields[resultFieldIndex].value = `**${this.challenger.username}** won! :trophy:`;
+        break;
+      case MatchResult.CHALLENGER_FORFEITED:
+        this.scoreboard.fields[resultFieldIndex].value = `**${this.challenger.username}> forfeited :expressionless:`;
+        break;
+      case MatchResult.OPPONENT_FORFEITED:
+        this.scoreboard.fields[resultFieldIndex].value = `**${this.opponent.username}>** forfeited :expressionless:`;
+        break;
+      case MatchResult.CHALLENGER_LEFT:
+        this.scoreboard.fields[resultFieldIndex].value = `**${this.challenger.username}>** left the match :(`;
+        break;
+      case MatchResult.OPPONENT_LEFT:
+        this.scoreboard.fields[resultFieldIndex].value = `**${this.opponent.username}>** left the match :(`;
+        break;
+    }
+
     this.challenger.send(this.generateScoreBoard());
     this.opponent.send(this.generateScoreBoard());
   }
