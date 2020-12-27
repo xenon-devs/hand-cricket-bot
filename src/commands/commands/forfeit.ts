9a157d2e8d59077efe80345b752084842b3a0d53
HarshKhandeparkar <mjkgoa@gmail.com>
2020-12-27 17:23:36 +0530

    fix: allow global match forfeit

--- src/commands/commands/forfeit.ts
+++ src/commands/commands/forfeit.ts
@@ -3,11 +3,13 @@ import { DiscordClient } from '../../util/discord-client';
 import { setCommand } from '../command';
 import { SinglePlayerMatch } from '../../game/single-player/single-player';
 import { MultiPlayerMatch } from '../../game/multi-player/multi-player';
+import { GlobalMatch } from '../../game/global/global';
 
 export function setForfeit(
   client: DiscordClient,
   current1PMatches: Map<string, SinglePlayerMatch>,
-  current2PMatches: Map<string, MultiPlayerMatch>
+  current2PMatches: Map<string, MultiPlayerMatch>,
+  currentGlobalMatches: Map<string, GlobalMatch>
 ) {
   return setCommand(
     client,
@@ -17,6 +19,7 @@ export function setForfeit(
     (msg: Message) => {
       current1PMatches.forEach(match => match.forfeit(msg.author.id));
       current2PMatches.forEach(match => match.forfeit(msg.author.id));
+      currentGlobalMatches.forEach(match => match.forfeit(msg.author.id));
     }
   )
 }
