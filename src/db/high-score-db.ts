997ff6fbde8d7989c3be6fac769004cbd7f42602
HarshKhandeparkar <mjkgoa@gmail.com>
2020-12-28 16:42:29 +0530

    refactor: use BaseDB everywhere

--- src/db/high-score-db.ts
+++ src/db/high-score-db.ts
@@ -1,5 +1,6 @@
 import { readFileSync, writeFileSync, existsSync } from 'fs';
 import { join } from 'path';
+import { BaseDB } from './base-db';
 
 export interface IHighScore {
   score: number,
@@ -18,31 +19,15 @@ export const emptyHighScoreDB: IHighScoreDB = {
 
 export const MAX_SCORES_STORED = 10;
 
-export class HighScoreDB {
-  dbLoc: string;
-  HIGH_SCORE_JSON: string;
-  dbOpsQueue: (() => void)[] = [];
-
-  constructor(dbLoc: string) {
-    this.dbLoc = dbLoc;
-    this.HIGH_SCORE_JSON = join(this.dbLoc, 'high-scores.json');
-
-    if (!existsSync(this.HIGH_SCORE_JSON)) writeFileSync(this.HIGH_SCORE_JSON, JSON.stringify(emptyHighScoreDB));
-    else {
-      const currentDb = JSON.parse(readFileSync(this.HIGH_SCORE_JSON).toString());
-
-      if (typeof currentDb.singlePlayer === 'undefined') currentDb.singlePlayer = emptyHighScoreDB.singlePlayer;
-      if (typeof currentDb.multiPlayer === 'undefined') currentDb.multiPlayer = emptyHighScoreDB.multiPlayer;
-
-      writeFileSync(this.HIGH_SCORE_JSON, JSON.stringify(currentDb));
-    }
-  }
+export class HighScoreDB extends BaseDB<IHighScoreDB> {
+  dbDefaults = emptyHighScoreDB;
+  dbFileName = 'high-scores';
 
   private addNewHighScore(
+    currentDb: IHighScoreDB,
     newHigh: IHighScore,
     multiplayer: boolean
   ) {
-    const currentDb: IHighScoreDB = JSON.parse(readFileSync(this.HIGH_SCORE_JSON).toString());
     const currentList = currentDb[multiplayer ? 'multiPlayer' : 'singlePlayer'];
 
     currentList.push(newHigh);
@@ -52,24 +37,18 @@ export class HighScoreDB {
 
     currentDb[multiplayer ? 'multiPlayer' : 'singlePlayer'] = newList;
 
-    writeFileSync(this.HIGH_SCORE_JSON, JSON.stringify(currentDb));
+    return currentDb;
   }
 
   addHighScore(
     newHigh: IHighScore,
     multiplayer: boolean
   ) {
-    this.dbOpsQueue.push(() => this.addNewHighScore(newHigh, multiplayer));
-    this.clearQueue();
+    this.dbOpsQueue.push((currentDb: IHighScoreDB) => this.addNewHighScore(currentDb, newHigh, multiplayer));
+    this.clearOPQueue();
   }
 
   getScores(): IHighScoreDB {
-    return JSON.parse(readFileSync(this.HIGH_SCORE_JSON).toString());
-  }
-
-  private clearQueue() {
-    for (let i = 0; i < this.dbOpsQueue.length; i++) {
-      (this.dbOpsQueue.shift())();
-    }
+    return this.readDB();
   }
 }
--- src/db/matches-db.ts
+++ src/db/matches-db.ts
@@ -1,5 +1,4 @@
-import { readFileSync, writeFileSync, existsSync } from 'fs';
-import { join } from 'path';
+import { BaseDB } from './base-db';
 
 export interface IMatchesDBStructure {
   singlePlayer: number,
@@ -13,54 +12,29 @@ export const emptyMatchesDB: IMatchesDBStructure = {
   global: 0
 }
 
-export class MatchesDB {
-  dbLoc: string;
-  MATCHES_JSON: string;
-  dbOpsQueue: (() => void)[] = [];
-
-  constructor(dbLoc: string) {
-    this.dbLoc = dbLoc;
-    this.MATCHES_JSON = join(this.dbLoc, 'matches.json');
-
-    if (!existsSync(this.MATCHES_JSON)) writeFileSync(this.MATCHES_JSON, JSON.stringify(emptyMatchesDB));
-    else {
-      const currentDb = JSON.parse(readFileSync(this.MATCHES_JSON).toString());
-
-      if (typeof currentDb.singlePlayer === 'undefined') currentDb.singlePlayer = emptyMatchesDB.singlePlayer;
-      if (typeof currentDb.multiPlayer === 'undefined') currentDb.multiPlayer = emptyMatchesDB.multiPlayer;
-      if (typeof currentDb.global === 'undefined') currentDb.global = emptyMatchesDB.global;
-
-      writeFileSync(this.MATCHES_JSON, JSON.stringify(currentDb));
-    }
-  }
+export class MatchesDB extends BaseDB<IMatchesDBStructure> {
+  dbDefaults = emptyMatchesDB;
+  dbFileName = 'matches';
 
   private addNewMatch(
+    currentDb: IMatchesDBStructure,
     matchType: 'singlePlayer' | 'multiPlayer' | 'global'
   ) {
-    const currentDb: IMatchesDBStructure = JSON.parse(readFileSync(this.MATCHES_JSON).toString());
     const currentMatches = currentDb[matchType];
-
     const newMatches = currentMatches + 1;
-
     currentDb[matchType] = newMatches;
 
-    writeFileSync(this.MATCHES_JSON, JSON.stringify(currentDb));
+    return currentDb;
   }
 
   addMatch(
     matchType: 'singlePlayer' | 'multiPlayer' | 'global'
   ) {
-    this.dbOpsQueue.push(() => this.addNewMatch(matchType));
-    this.clearQueue();
+    this.dbOpsQueue.push((currentDb: IMatchesDBStructure) => this.addNewMatch(currentDb, matchType));
+    this.clearOPQueue();
   }
 
   getMatches(): IMatchesDBStructure {
-    return JSON.parse(readFileSync(this.MATCHES_JSON).toString());
-  }
-
-  private clearQueue() {
-    for (let i = 0; i < this.dbOpsQueue.length; i++) {
-      (this.dbOpsQueue.shift())();
-    }
+    return this.readDB();
   }
 }
