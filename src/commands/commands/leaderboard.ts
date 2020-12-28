94a60315915fd8175b15871284c95fb8a66b0a1b
HarshKhandeparkar <mjkgoa@gmail.com>
2020-12-28 12:11:12 +0530

    feat: leaderboard reaction menu

--- src/commands/commands/leaderboard.ts
+++ src/commands/commands/leaderboard.ts
@@ -1,7 +1,8 @@
-import { MessageEmbed, Message } from 'discord.js';
+import { MessageEmbed, Message, TextChannel, DMChannel } from 'discord.js';
 import { DiscordClient } from '../../util/discord-client';
 import { setCommand } from '../command';
 import { IHighScore } from '../../../src/db/high-score-db';
+import { ReactionMenu } from '@xenon-devs/discordjs-reaction-menu';
 
 const rankMappingFunction = (score: IHighScore, i: number) => {
   return {
@@ -23,30 +24,54 @@ export function setLeaderboard(client: DiscordClient) {
       const singlePlayerRankFields = scores.singlePlayer.map(rankMappingFunction);
       const multiPlayerRankFields = scores.multiPlayer.map(rankMappingFunction);
 
-      msg.channel.send(
-        new MessageEmbed()
-        .setTitle(`Hand Cricketer Leaderboard`)
-        .setDescription(`Leaderboard of all Hand Cricketer players on discord.`)
-        .addField(
-          'Single Player',
-          scores.singlePlayer.length > 0 ?
-            'Following is the list of top single player high scores (in test match).' :
-            'No scores recorded yet.',
-          false)
-        .addFields(singlePlayerRankFields)
-        .addField(
-          'Multi Player',
-          scores.multiPlayer.length > 0 ?
-            'Following is the list of top multi player high scores (in test match).' :
-            'No scores recorded yet.',
-          false
-        )
-        .addFields(multiPlayerRankFields)
-        .setThumbnail(client.user.displayAvatarURL())
-        .setColor('GREEN')
-        .setTimestamp()
-        .setFooter('By Team Xenon', 'https://raw.githubusercontent.com/xenon-devs/xen-assets/main/xen-inc/logo/xen-logo-black-bg.png')
+      const leaderboardMenu = new ReactionMenu(
+        [
+          {
+            pageEmbed:  new MessageEmbed()
+              .setTitle(`Hand Cricketer Leaderboard`)
+              .setDescription(`Leaderboard of all Hand Cricketer players on discord. Click on the emojis below to navigate.`)
+              .addFields([
+                {name: '1.', value: 'Single Player High Scores'},
+                {name: '2.', value: 'Multu Player High Scores'}
+              ])
+              .setThumbnail(client.user.displayAvatarURL())
+              .setColor('GREEN')
+              .setTimestamp()
+              .setFooter('By Team Xenon', 'https://raw.githubusercontent.com/xenon-devs/xen-assets/main/xen-inc/logo/xen-logo-black-bg.png')
+          },
+          {
+            pageEmbed:  new MessageEmbed()
+              .setTitle(`Hand Cricket Singleplayer High Scores`)
+              .setDescription(
+                scores.singlePlayer.length > 0 ?
+                  'Following is the list of top single player high scores (in test match).' :
+                  'No scores recorded yet.'
+              )
+              .addFields(singlePlayerRankFields)
+              .setThumbnail(client.user.displayAvatarURL())
+              .setColor('GREEN')
+              .setTimestamp()
+              .setFooter('By Team Xenon', 'https://raw.githubusercontent.com/xenon-devs/xen-assets/main/xen-inc/logo/xen-logo-black-bg.png')
+          },
+          {
+            pageEmbed:  new MessageEmbed()
+              .setTitle(`Hand Cricket Multiplayer High Scores`)
+              .setDescription(
+                scores.multiPlayer.length > 0 ?
+                  'Following is the list of top multi player high scores (in test match).' :
+                  'No scores recorded yet.'
+              )
+              .addFields(multiPlayerRankFields)
+              .setThumbnail(client.user.displayAvatarURL())
+              .setColor('GREEN')
+              .setTimestamp()
+              .setFooter('By Team Xenon', 'https://raw.githubusercontent.com/xenon-devs/xen-assets/main/xen-inc/logo/xen-logo-black-bg.png')
+          }
+        ],
+        <TextChannel | DMChannel>msg.channel
       )
+
+      leaderboardMenu.start(0);
 
       if (client.advertisement !== null) msg.channel.send(client.advertisement);
     }
