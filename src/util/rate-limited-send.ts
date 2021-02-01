b39594c4b07fa33c75d8bbb58f6f97b96336f9e1
HarshKhandeparkar <mjkgoa@gmail.com>
2021-02-01 11:03:38 +0530

    feat changes to rate limited send

--- src/util/rate-limited-send.ts
+++ src/util/rate-limited-send.ts
@@ -1,31 +1,44 @@
-import { TextChannel, MessageEmbed } from 'discord.js';
+import { TextChannel, MessageEmbed, User, Message, DMChannel } from 'discord.js';
 
 /**
  * key -> server id
  * value -> timestamp of the last sent message
  */
-const lastMessageTimeStamps: Map<string, number> = new Map();
+const lastMessageServerTimeStamps: Map<string, number> = new Map();
+/**
+ * key -> DM channel id
+ * value -> timestamp of the last sent message
+ */
+const lastMessageDMTimeStamps: Map<string, number> = new Map();
 /**
  * Number of miliseconds between between each message
  */
-const minimumDelay = 1000;
+const minimumDelay = 360;
 
 export async function send(
-  channel: TextChannel,
+  sendTo: TextChannel | DMChannel | User,
   content: string | number | MessageEmbed
 ) {
-  const lastSentTimestamp = lastMessageTimeStamps.has(channel.guild.id) ? lastMessageTimeStamps.get(channel.guild.id) : 0;
+  let lastSentTimestamp: number = 0;
+
+  if ('username' in sendTo) lastSentTimestamp = lastMessageDMTimeStamps.get(sendTo.dmChannel.id) || 0;
+  else if ('type' in sendTo && sendTo.type === 'dm') lastSentTimestamp = lastMessageDMTimeStamps.get(sendTo.id) || 0;
+  else lastSentTimestamp = lastMessageServerTimeStamps.get(sendTo.guild.id) || 0;
+
   const currentTime = new Date().getTime();
   const delay = currentTime - lastSentTimestamp;
 
-  return new Promise((resolve) => {
+  return new Promise((resolve: (msg: Message) => void) => {
     if (delay > minimumDelay) {
-      channel.send(content);
-      lastMessageTimeStamps.set(channel.guild.id, currentTime);
+      sendTo.send(content);
+
+      if ('username' in sendTo) lastMessageDMTimeStamps.set(sendTo.dmChannel.id, currentTime);
+      else if ('type' in sendTo && sendTo.type === 'dm') lastMessageDMTimeStamps.set(sendTo.id, currentTime);
+      else lastMessageServerTimeStamps.set(sendTo.guild.id, currentTime);
     }
     else setTimeout(
-      () => {
-        resolve(send(channel, content));
+      async () => {
+        resolve(await send(sendTo, content));
       }, minimumDelay - delay + 10
     )
   })
