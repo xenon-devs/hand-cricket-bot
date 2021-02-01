4bc227866aec912c076daa875ec705e1fa130b19
HarshKhandeparkar <mjkgoa@gmail.com>
2021-02-01 12:32:35 +0530

    fix: delete rate limit send code

--- src/util/rate-limited-send.ts
+++ src/util/rate-limited-send.ts
@@ -1,45 +1,11 @@
 import { TextChannel, MessageEmbed, User, Message, DMChannel } from 'discord.js';
 
-/**
- * key -> server id
- * value -> timestamp of the last sent message
- */
-const lastMessageServerTimeStamps: Map<string, number> = new Map();
-/**
- * key -> DM channel id
- * value -> timestamp of the last sent message
- */
-const lastMessageDMTimeStamps: Map<string, number> = new Map();
-/**
- * Number of miliseconds between between each message
- */
-const minimumDelay = 360;
+export type SendTo = TextChannel | DMChannel | User;
+export type Content = string | number | MessageEmbed;
 
 export async function send(
-  sendTo: TextChannel | DMChannel | User,
-  content: string | number | MessageEmbed
+  sendTo: SendTo,
+  content: Content
 ) {
-  let lastSentTimestamp: number = 0;
 
-  if ('username' in sendTo) lastSentTimestamp = lastMessageDMTimeStamps.get(sendTo.dmChannel.id) || 0;
-  else if ('type' in sendTo && sendTo.type === 'dm') lastSentTimestamp = lastMessageDMTimeStamps.get(sendTo.id) || 0;
-  else lastSentTimestamp = lastMessageServerTimeStamps.get(sendTo.guild.id) || 0;
-
-  const currentTime = new Date().getTime();
-  const delay = currentTime - lastSentTimestamp;
-
-  return new Promise((resolve: (msg: Message) => void) => {
-    if (delay > minimumDelay) {
-      sendTo.send(content);
-
-      if ('username' in sendTo) lastMessageDMTimeStamps.set(sendTo.dmChannel.id, currentTime);
-      else if ('type' in sendTo && sendTo.type === 'dm') lastMessageDMTimeStamps.set(sendTo.id, currentTime);
-      else lastMessageServerTimeStamps.set(sendTo.guild.id, currentTime);
-    }
-    else setTimeout(
-      async () => {
-        resolve(await send(sendTo, content));
-      }, minimumDelay - delay + 10
-    )
-  })
 }
