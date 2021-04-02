2ff3c32cda8271b4a94393b666405b47d3451c71
eniamza <65225917+Eniamza@users.noreply.github.com>
2021-04-02 20:55:08 +0600

    Update help.ts

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -37,7 +37,7 @@ In the first half of October, we hosted nearly 2000 single and multi player matc
       if (client.dblIntegration) {
         helpEmbed.addField('Add bot to your server', `[Click to Add](https://top.gg/bot/${client.user.id})`, true);
         helpEmbed.addField('Liked it?', `[Click to Vote](https://top.gg/bot/${client.user.id}/vote)`, true);
-        helpEmbed.addField('Want to Swap cryptos with ease?', `[Try SwapCC](https://discordbotlist.com/bots/swapcc)`, true);
+        helpEmbed.addField('Want to Swap cryptos with ease?', `[Try SwapCC](https://top.gg/bot/812942138622345277)`, true);
 
         const botStats = await client.dbl.getBot(client.user.id);
         if (botStats.support) helpEmbed.addField('Support Server', `[Click to Join](https://discord.gg/${botStats.support})`, true);
